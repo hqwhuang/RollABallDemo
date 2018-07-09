909cdff9f1f19ba08189f99516b7520572b73144
hqwhuang <hqwhuange@gmail.com>
2018-07-09 22:43:54 +0800

    Upload demo

---
+++ Roll a ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	public float MoveSpeed;
+	// Update is called once per frame
+    private int count;
+    void Start(){
+        count = 0;
+    }
+	void Update () {
+        count++;
+		transform.Rotate (new Vector3(15, 30, 45) * Time.deltaTime);
+        if(count/100%2==0)
+            transform.Translate(Vector3.forward * MoveSpeed * Time.deltaTime, Space.World);
+        else
+            transform.Translate(Vector3.back * MoveSpeed * Time.deltaTime, Space.World);
+	}
+}
